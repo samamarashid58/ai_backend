6b588303de8537428af260e232a65c1c837d0c3a
samamarashid58 <samamarashid582000@gmail.com>
2026-03-28 22:46:47 +0500

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
