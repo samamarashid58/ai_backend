914076d7785f1ba8bc498e6a5505b6f7714b8eac
samamarashid58 <samamarashid582000@gmail.com>
2026-03-28 22:01:22 +0500

    api changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 // ✅ Middlewares
 app.use(cors({ origin: "*" })); // Allow all origins for global access
